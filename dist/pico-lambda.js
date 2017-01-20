67561c4f6d77bf0e271dcc18967be2c844cf3d52
mattmcfarland <contact@mattmcfarland.com>
2017-01-20 16:51:42 -0500

    update build steps

--- dist/pico-lambda.js
+++ dist/pico-lambda.js
@@ -1,17 +1,18 @@
-const whitelist = ['concat', 'every', 'filter', 'find', 'includes', 'map', 'reduce', 'reduceRight', 'slice', 'some']
-const p = {}
 
-Object
+const p = Object
   .getOwnPropertyNames(Array.prototype)
-  .filter(s => whitelist.includes(s))
-  .forEach(method => {
+  .filter(s => ['concat', 'every', 'filter', 'find', 'includes', 'map', 'reduce', 'reduceRight', 'slice', 'some'].includes(s))
+  .reduce((p, method) => {
     p[method] = fn => (a, ...args) => a[method](fn, ...args)
-  })
+    return p
+  }, {
 
-p.cons = a => b => [a, ...b]
-p.uniq = a => [...new Set(a)]
-p.compose = (...fns) => data => fns.reduceRight((value, fn) => fn(value), data)
-p.pipe = (...fns) => data => fns.reduce((value, fn) => fn(value), data)
+    cons: a => b => [a, ...b],
+
+    compose: (...fns) => data => fns.reduceRight((value, fn) => fn(value), data),
+
+    pipe: (...fns) => data => fns.reduce((value, fn) => fn(value), data)
+  })
 
 if (typeof window !== 'undefined') window.PicoLambda = p
 else module.exports = p
